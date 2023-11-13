a140dc9e251d6814f50bea788186728d88ec5432
Gabriela Rodrigues <gabriela.matias@sou.inteli.edu.br>
2023-11-13 00:57:21 -0300

    create files

---
+++ m8-ec-2023/docs/ponderada1/resolution.md
@@ -0,0 +1,17 @@
+---
+sidebar_position: 1
+---
+
+# Resolução
+
+## 1. Requisito Funcional:
+O robô de serviço deve ser capaz de receber solicitações, com base na demanda do usuário, sobre a requisição de uma peça ou ferramenta, de forma geral, um elemento que pode ser obtido no almoxarifado. E com base nessa solicitação deve saber orientar o usuário sobre o local para obter esse elemento.
+
+## 2. Requisito não Funcional:
+O chatbot deve ser capaz de compreender e responder às solicitações dos usuários no contexto do almoxarifado da Ambev de forma eficiente, considerando a métrica de eficiência composta por precisão e rapidez.
+
+### 2.1. Precisão:
+O chatbot deve fornecer respostas precisas que atendam às solicitações dos usuários no contexto do almoxarifado. A precisão será medida pela proporção de respostas corretas em relação ao número total de solicitações de usuários.
+
+### 2.2. Rapidez:
+O chatbot deve responder às solicitações dos usuários de forma rápida, minimizando o tempo de resposta. A rapidez será medida pelo tempo médio de resposta do chatbot a partir do momento em que a solicitação é recebida até o momento em que a resposta é fornecida ao usuário.
---
+++ m8-ec-2023/src/pages/index.js
@@ -0,0 +1,46 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+      <div>
+        <img style={{width:'20%', borderRadius:"50%"}} src="https://media.licdn.com/dms/image/D4D03AQF2YywgzN-rDw/profile-displayphoto-shrink_800_800/0/1665767974674?e=1705536000&v=beta&t=J8C9u6S945SVNODPfG_mF_O2NlnoGyXvt3STjvA5Enw"/>
+      </div>
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Acesse a Documentação
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
