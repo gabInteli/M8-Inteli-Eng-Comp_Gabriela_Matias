d9125236f165daeced4994dc81c38476aa247741
Gabriela Rodrigues <gabriela.matias@sou.inteli.edu.br>
2023-11-21 02:44:14 -0300

    fix bugs deploy docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/M8-Inteli-Eng-Comp_Gabriela_Matias/',
+  baseUrl: '/M8-Inteli-Eng-Comp_Gabriela_Matias',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
